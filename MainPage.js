eb250c79c11368910021b427ba4a1b0449d8a164
EkaterinaKladova <71469565+EkaterinaKladova@users.noreply.github.com>
2022-04-13 17:00:47 -0400

    adding comments

--- MainPage.js
+++ MainPage.js
@@ -1,4 +1,4 @@
-
+/* initializing firebase */
 var admin = require("firebase-admin");
 
 var serviceAccount = require("path/to/serviceAccountKey.json");
@@ -6,13 +6,15 @@ var serviceAccount = require("path/to/serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+/* upon pressing login button */
 function login(){
     const username = document.getElementById("login-user").value.toLowerCase().trim();
     
     const password = document.getElementById("login-password").value;
     
     
-      
+    /* checking that both password and username are inputted */
     if(username==""){
         alert("No username entered");
     }else if(password==""){
